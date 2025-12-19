06e912259d49f3db5e266550aa7784e5544a7001
AldoQuispeDev <aldoquispelocumber029@gmail.com>
2025-12-19 03:51:20 -0500

    Actualizando frontend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
-  withCredentials: true, // necesario para JWT en cookies
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+  withCredentials: true,
 });
 
 export default api;
